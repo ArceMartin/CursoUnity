a949a5f37c163b58ebfca90f3fb279d8d35e0179
Martin Arce <13899640+ArceMartin@users.noreply.github.com>
2022-07-21 09:57:03 -0700

    Hasta clase de 14 de junio. Se completo metodo de movimiento horizontal para el player.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,23 +9,35 @@ public class PlayerController : MonoBehaviour
   private Rigidbody2D rb;
   public float velocidad = 10;
 
-  private void awake() 
+  private void Awake() 
   {
     rb = GetComponent<Rigidbody2D>();
   }
 
   void Start(){}  // Se llama antes de la actualización del primer fotograma
-  void Update(){} // Se llama una vez por fotograma
+  void Update()   // Se llama una vez por fotograma
+  {
+    Movimiento();
+  } 
 
   private void Movimiento() 
   {
     float x = Input.GetAxis("Horizontal"); // Recupera el eje input de Unity
     float y = Input.GetAxis("Vertical");   // Recupera el eje input de Unity
+
+    Vector2 direccion = new Vector2(x, y); // Creamos un vector con los inputs
+
+    Caminar(direccion);
   }
 
-  private void Caminar() 
+  private void Caminar(Vector2 direccion) 
   {
-    
+    // Al caminar tomamos en cuenta el input horizontal * la variable velocidad
+    // La velocidad vertical permanece igual
+    rb.velocity = new Vector2(
+      direccion.x * velocidad,
+      rb.velocity.y
+      );
   }
 
 }
