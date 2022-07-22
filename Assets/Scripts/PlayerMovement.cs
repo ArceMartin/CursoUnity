4a0c3f3153537397d01e9f2e85cd1685161e9a36
Martin Arce <13899640+ArceMartin@users.noreply.github.com>
2022-07-22 12:33:19 -0700

    Hasta clase del 15/jun/22. Se agrego metodo de caminar con Input System

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,20 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
   public Rigidbody2D rbody;
   public float jumpForce = 10;
+  public float walkSpeed = 10;
+  private float currentSpeed = 0;
 
   void Start(){}  // Se llama a Start antes de la actualización del primer fotograma
-  void Update(){} // Update se llama una vez por fotograma
+  void Update() // Update se llama una vez por fotograma
+  {
+    rbody.velocity = new Vector2(currentSpeed, rbody.velocity.y);
+  }
 
   // Metodo OnJump se ejecuta cuando el Input Action Asset detecta el evento Jump
   // por medio de los inputs que especificamos (up, w, space)
   private void OnJump() 
   {
-    Debug.Log("Jump!");
-    rbody.velocity = new Vector2(rbody.velocity.x, jumpForce);
+    /* TAREA: Implementar una restriccion para no poder saltar en el aire */
+    if (rbody.velocity.y == 0) 
+    {
+      rbody.velocity = new Vector2(rbody.velocity.x, jumpForce);
+    }
+  }
+
+  // Metodo OnMove se ejecuta cuando el Input Action Asset detecta el evento Move
+  // por medio de los inputs que especificamos (left, right, a, d)
+  private void OnMove(InputValue inputValue) 
+  {
+    // Recuperamos el input izquierda o derecha -1 / 0 / 1
+    float moveValue = inputValue.Get<float>();
+    // Actualizamos la velocidad con el valor 
+    // rbody.velocity = new Vector2(moveValue*walkSpeed, rbody.velocity.y);
+    currentSpeed = moveValue * walkSpeed;
   }
 }
