5e9758f488c125e1d42c01a7a136f9dc1d7a6691
Martin Arce <13899640+ArceMartin@users.noreply.github.com>
2022-07-21 20:00:59 -0700

    Se completo movimiento y salto primera version

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,7 +7,9 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
   private Rigidbody2D rb;
-  public float velocidad = 10;
+  private Vector2 direccion;   // Vector para guardar los inputs en cada momento
+  public float velocidad = 10; // velocidad de caminar
+  public float fuerzaSalto = 5; // fuerza del salto
 
   private void Awake() 
   {
@@ -24,20 +26,51 @@ public class PlayerController : MonoBehaviour
   {
     float x = Input.GetAxis("Horizontal"); // Recupera el eje input de Unity
     float y = Input.GetAxis("Vertical");   // Recupera el eje input de Unity
+    direccion = new Vector2(x, y); // Vector con los inputs
 
-    Vector2 direccion = new Vector2(x, y); // Creamos un vector con los inputs
+    Caminar();
+    DuracionSalto();
 
-    Caminar(direccion);
+    // ejecuta este bloque si la barra espaciadora esta presionada
+    if (Input.GetKeyDown(KeyCode.Space))
+    {
+      Saltar();
+    }
   }
 
-  private void Caminar(Vector2 direccion) 
+  private void Caminar() 
   {
     // Al caminar tomamos en cuenta el input horizontal * la variable velocidad
     // La velocidad vertical permanece igual
-    rb.velocity = new Vector2(
-      direccion.x * velocidad,
-      rb.velocity.y
-      );
+    rb.velocity = new Vector2(direccion.x * velocidad, rb.velocity.y);
   }
+
+  private void DuracionSalto() 
+  {
+    // Personaje cayendo
+    if (rb.velocity.y < 0) 
+    {
+      // Esta parte depende de la experiencia física que queremos dar al juego
+      rb.velocity += Vector2.up 
+        * Physics2D.gravity.y // nos da 'gravedad'
+        * (2.5f - 1)          // cambiamos estos valores a gusto
+        * Time.deltaTime;     // conforme pasa el tiempo, aumenta velocidad de caida
+    }
+    // Personaje saltando pero ya dejamos de presionar barra espaciadora
+    else if (rb.velocity.y > 0 && Input.GetKey(KeyCode.Space))
+    {
+      rb.velocity += Vector2.up * Physics2D.gravity.y * (2.5f - 1) * Time.deltaTime; // igual al caso anterior
+    }
+  }
+
+  private void Saltar() 
+  {
+    // ponemos en 0 la velocidad vertical inicial (inicio del salto)
+    rb.velocity = new Vector2(rb.velocity.x, 0); 
+
+    // ponemos la velocidad vertical proporcional a la fuerza del salto
+    rb.velocity += Vector2.up * fuerzaSalto;
+  }
+
 
 }
