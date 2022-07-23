fefd5c0c0fcd7b23f7e87ed2b0dec2e403ec181f
Martin Arce <13899640+ArceMartin@users.noreply.github.com>
2022-07-23 14:29:48 -0700

    Hasta clase de 16/jun/22. Condicion de colision con suelo. Se incluyen archivos de animacion.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,15 +6,26 @@ using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
 {
   public Rigidbody2D rbody;
+
+  [Header("Estadisticas")]
   public float jumpForce = 10;
   public float walkSpeed = 10;
   private float currentSpeed = 0;
+
+  [Header("Colisiones")]
+  public Vector2 abajo = new Vector2(0, -0.79f); // para conocer la posicion de los pies
+  public float radioColision = 0.1f; // separacion entre el pie y el piso para la colision
+  public  LayerMask layerPiso; // La capa con la que existe la colision
+
+  [Header("Booleanos / Banderas")] 
+  public bool enSuelo;
 
   void Start(){}  // Se llama a Start antes de la actualización del primer fotograma
   void Update() // Update se llama una vez por fotograma
   {
     // Permite al personaje permanecer con su velovidad al caer al suelo
     rbody.velocity = new Vector2(currentSpeed, rbody.velocity.y);
+    TocarSuelo();
   }
 
   // Metodo OnJump se ejecuta cuando el Input Action Asset detecta el evento Jump
@@ -22,9 +33,14 @@ public class PlayerMovement : MonoBehaviour
   private void OnJump() 
   {
     /* TAREA: Implementar una restriccion para no poder saltar en el aire */
-    if (rbody.velocity.y == 0) 
+    /*if (rbody.velocity.y == 0) 
     {
       rbody.velocity = new Vector2(rbody.velocity.x, jumpForce);
+    }*/
+    if (enSuelo) 
+    {
+      Debug.Log("Jump!");
+      rbody.velocity = new Vector2(rbody.velocity.x, jumpForce); // Aplica fuerza del salto
     }
   }
 
@@ -45,5 +61,13 @@ public class PlayerMovement : MonoBehaviour
       transform.localScale = new Vector2(-1 * transform.localScale.x, transform.localScale.y);
     }
 
+  }
+
+  private void TocarSuelo() 
+  {
+    // determina coordenadas de los pies
+    Vector2 pies = (Vector2)transform.position + abajo; 
+    // checa si existe colision
+    enSuelo = Physics2D.OverlapCircle(pies, radioColision, layerPiso); 
   }
 }
