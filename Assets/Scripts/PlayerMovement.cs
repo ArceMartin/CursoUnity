9064cbc8509b747fba5e1302d72bdc8647e29c8b
Martin Arce <13899640+ArceMartin@users.noreply.github.com>
2022-07-28 12:51:26 -0700

    Animaciones de ataque

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,3 +1,7 @@
+// Taller de Videojuegos Unity 2022-4
+// Martín Arce
+// Matricula: 1103883
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -9,7 +13,8 @@ public class PlayerMovement : MonoBehaviour
   public  Rigidbody2D rbody;
   public  Animator anim;
   private Vector2 velocityPreDash;
-  public CinemachineVirtualCamera cm;
+  public  CinemachineVirtualCamera cm;
+  private Vector2 direccionAtaque = new Vector2(1f,0f);
 
   [Header("Estadisticas")]
   public  float jumpForce = 6;
@@ -26,9 +31,10 @@ public class PlayerMovement : MonoBehaviour
   public  bool enSuelo;
   public  bool segundoSaltoDisponible;
   public  bool haciendoDash = false; // Nos indica si se encuentra haciendo dash
-  public  bool puedeDash;    // Nos limita si se puede hacer dash
+  private bool puedeDash = true;    // Nos limita si se puede hacer dash
   public  bool puedoMover = true;
   public  bool vibrando = false;
+  public  bool atacando = false; // Indica si el personaje esta atacando
 
   private void Awake() 
   {
@@ -36,9 +42,11 @@ public class PlayerMovement : MonoBehaviour
       .GetComponent<CinemachineVirtualCamera>();
   }
 
-  void Start(){}  // Una vez antes de la actualización del primer fotograma
+  /*Una vez antes de la actualización del primer fotograma*/
+  void Start(){}  
   
-  void Update() // Una vez por fotograma
+  /*Una vez por fotograma*/
+  void Update() 
   {
     // if (puedoMover && !haciendoDash) 
     if (puedoMover) 
@@ -89,6 +97,9 @@ public class PlayerMovement : MonoBehaviour
     // Recuperamos el input izquierda o derecha -1 / 0 / 1
     float moveValue = inputValue.Get<float>();
     currentSpeed = moveValue * walkSpeed;
+    // Copiamos input horizontal al vector direccionAtaque
+    // if (moveValue != 0) 
+      direccionAtaque.x = moveValue;
 
     // Logica para cambiar direccion del sprite
     if (moveValue * transform.localScale.x < 0) // input y direccion opuestos
@@ -100,14 +111,14 @@ public class PlayerMovement : MonoBehaviour
   private void OnDash()  
   {
     // Verifica que el player esta en movimiento
-    if (rbody.velocity != Vector2.zero && !haciendoDash) 
+    if (rbody.velocity != Vector2.zero && puedeDash) 
     {
       // Efecto de ripple centrado en el player
       Vector3 posicionJugador = Camera.main.WorldToViewportPoint(transform.position);
       Camera.main.GetComponent<RippleEffect>().Emit(posicionJugador);
 
       // Efecto de agitar camara
-      StartCoroutine(AgitarCamara());
+      StartCoroutine(AgitarCamara(0.3f));
 
       // Guarda vector velocidad actual
       velocityPreDash = rbody.velocity; 
@@ -124,15 +135,13 @@ public class PlayerMovement : MonoBehaviour
   }
 
   /*Corutina para modificar los parámetros del ruido de camara al hacer dash*/
-  private IEnumerator AgitarCamara() 
+  private IEnumerator AgitarCamara(float tiempo) 
   {
     vibrando = true;
     CinemachineBasicMultiChannelPerlin cmNoise;
     cmNoise = cm.GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>();
-
-    // Cambiamos la amplitud a 10 por 0.3 segundos
     cmNoise.m_AmplitudeGain  = 10;
-    yield return new WaitForSeconds(0.3f);
+    yield return new WaitForSeconds(tiempo);
     cmNoise.m_AmplitudeGain  = 0;
     vibrando = false;
   }
@@ -142,33 +151,51 @@ public class PlayerMovement : MonoBehaviour
   {
     rbody.gravityScale = 0;
     haciendoDash = true;
+    puedeDash = false;
     anim.SetBool("dash", true); 
-
     yield return new WaitForSeconds(0.5f);
     rbody.gravityScale = 1;
     anim.SetBool("dash", false);
     rbody.velocity = velocityPreDash;
-  }
-
-  /*Esta corutina limita el dash con un tiempo de espera de 5 segundos*/
-  private IEnumerator LimitaDash() 
-  {
-    yield return new WaitForSeconds(5f);
     haciendoDash = false;
   }
 
-  // Usamos propiedad de Polimorfismo 
-  // Segunda corutina identica pero que admite un argumento
-/*  private IEnumerator AgitarCamara(float tiempo) 
+  /*Corutina que limita el dash con un tiempo de espera de 5 segundos*/
+  private IEnumerator LimitaDash() 
   {
-    vibrando = true;
-    CinemachineBasicMultiChannelPerlin cmNoise;
-    cmNoise = cm.GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>();
+    yield return new WaitForSeconds(5f);
+    puedeDash = true;
+  }
 
-    // Cambiamos la amplitud a 10 por 0.3 segundos
-    cmNoise.m_AmplitudeGain  = 10;
-    yield return new WaitForSeconds(tiempo);
-    cmNoise.m_AmplitudeGain  = 0;
-    vibrando = false;
-  }*/
+  // Metodo OnAttack se ejecuta cuando el Input Action Asset detecta el evento Attack
+  // por medio del input que especificamos (Z)
+  private void OnAttack() 
+  {
+    if (!atacando && !haciendoDash) 
+    {
+      atacando = true;
+      anim.SetBool("ataque", true);
+      anim.SetFloat("ataqueX", direccionAtaque.x);
+      anim.SetFloat("ataqueY", direccionAtaque.y);
+      Debug.Log($"x = {direccionAtaque.x}");
+      Debug.Log($"y = {direccionAtaque.y}");
+    }
+  }
+
+  private void FinalizarAtaque() 
+  {
+    anim.SetBool("ataque", false);
+    atacando = false;
+  }
+
+  // Metodo OnLook se ejecuta cuando el Input Action Asset detecta el evento Look
+  // por medio del input que especificamos (arriba, abajo, w, s)
+  private void OnLook(InputValue inputValue) 
+  {
+    // Recuperamos el valor del input (-1, 0 o 1)
+    float lookValue = inputValue.Get<float>();
+    // Debug.Log("Look!" + lookValue);
+    // Asignamos valor a vector direccionAtaque en base al input
+    direccionAtaque.y = lookValue;
+  }
 }
